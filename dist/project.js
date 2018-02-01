80e5fb2a5336be6682ed0ed70f1313eb03ce807a
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-31 20:53:15 -0600

    printing is not matching to what is in partial

--- dist/project.js
+++ dist/project.js
@@ -1327,3 +1327,299 @@
 }) (angular.module ('DoctorsAndPatients', ['ngRoute']));
 
 
+
+//====================================================================================================================
+// Module:    DoctorsAndPatients
+// Optimized: Yes
+// File:      ./app/app.js
+//====================================================================================================================
+
+(function (module) {
+
+  "use strict";
+
+  module
+  .constant('FBUrl','https://doctors-and-patie.firebaseio.com/')
+  .config($routeProvider => {
+      $routeProvider
+      .when("/",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl" 
+      })
+      .when("/doctors",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl"
+
+      })
+      .when("/patients",{
+          templateUrl: "partials/patients.html",
+          controller:"doctorsAndPatientsCtrl"
+      })
+      .otherwise("/");
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/controllers/doctorsAndPatientsCtrl.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.controller("doctorsAndPatientsCtrl",function($scope, routeFactory){
+      routeFactory.getDoctors()
+      .then((docObjects)=>{
+          $scope.doctors = docObjects;
+          let keys = Object.keys(docObjects);
+          console.log("keys",keys);
+
+
+          console.log("doc Objects", docObjects);
+
+          docObjects.forEach( (object)=> console.log(object,"object"));
+      });
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/factories/drPatFactory.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.factory("routeFactory",function(FBUrl, $q,$http){
+      return{
+          getDoctors : () =>{
+              console.log(FBUrl, "fbURL");
+              return $q((resolve,reject)=>{
+                  $http
+                  .get(`${FBUrl}/doctors.json`)
+                  .then((data)=>{
+                      console.log("data",data.data);
+                      resolve(data.data);
+                  });
+          });
+       }
+      };
+  });
+
+}) (angular.module ('DoctorsAndPatients', ['ngRoute']));
+
+
+
+//====================================================================================================================
+// Module:    DoctorsAndPatients
+// Optimized: Yes
+// File:      ./app/app.js
+//====================================================================================================================
+
+(function (module) {
+
+  "use strict";
+
+  module
+  .constant('FBUrl','https://doctors-and-patie.firebaseio.com/')
+  .config($routeProvider => {
+      $routeProvider
+      .when("/",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl" 
+      })
+      .when("/doctors",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl"
+
+      })
+      .when("/patients",{
+          templateUrl: "partials/patients.html",
+          controller:"doctorsAndPatientsCtrl"
+      })
+      .otherwise("/");
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/controllers/doctorsAndPatientsCtrl.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.controller("doctorsAndPatientsCtrl",function($scope, routeFactory){
+      routeFactory.getDoctors()
+      .then((docObjects)=>{
+          $scope.doctors = docObjects;
+          let keys = Object.keys(docObjects);
+          console.log("keys",keys);
+
+
+          console.log("doc Objects", docObjects);
+
+          docObjects.forEach( (object)=> console.log(object,"object"));
+      });
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/factories/drPatFactory.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.factory("routeFactory",function(FBUrl, $q,$http){
+      return{
+          getDoctors : () =>{
+              console.log(FBUrl, "fbURL");
+              return $q((resolve,reject)=>{
+                  $http
+                  .get(`${FBUrl}/doctors.json`)
+                  .then((data)=>{
+                      console.log("data",data.data);
+                      resolve(data.data);
+                  });
+          });
+       }
+      };
+  });
+
+}) (angular.module ('DoctorsAndPatients', ['ngRoute']));
+
+
+
+//====================================================================================================================
+// Module:    DoctorsAndPatients
+// Optimized: Yes
+// File:      ./app/app.js
+//====================================================================================================================
+
+(function (module) {
+
+  "use strict";
+
+  module
+  .constant('FBUrl','https://doctors-and-patie.firebaseio.com/')
+  .config($routeProvider => {
+      $routeProvider
+      .when("/",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl" 
+      })
+      .when("/doctors",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl"
+
+      })
+      .when("/patients",{
+          templateUrl: "partials/patients.html",
+          controller:"doctorsAndPatientsCtrl"
+      })
+      .otherwise("/");
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/controllers/doctorsAndPatientsCtrl.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.controller("doctorsAndPatientsCtrl",function($scope, routeFactory){
+      routeFactory.getDoctors()
+      .then((docObjects)=>{
+          $scope.doctors = docObjects;
+          let keys = Object.keys(docObjects);
+          console.log("keys",keys);
+
+
+          console.log("doc Objects", docObjects);
+
+          docObjects.forEach( (object)=> console.log(object,"object"));
+      });
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/factories/drPatFactory.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.factory("routeFactory",function(FBUrl, $q,$http){
+      return{
+          getDoctors : () =>{
+              console.log(FBUrl, "fbURL");
+              return $q((resolve,reject)=>{
+                  $http
+                  .get(`${FBUrl}/doctors.json`)
+                  .then((data)=>{
+                      console.log("data",data.data);
+                      resolve(data.data);
+                  });
+          });
+       }
+      };
+  });
+
+}) (angular.module ('DoctorsAndPatients', ['ngRoute']));
+
+
+
+//====================================================================================================================
+// Module:    DoctorsAndPatients
+// Optimized: Yes
+// File:      ./app/app.js
+//====================================================================================================================
+
+(function (module) {
+
+  "use strict";
+
+  module
+  .constant('FBUrl','https://doctors-and-patie.firebaseio.com/')
+  .config($routeProvider => {
+      $routeProvider
+      .when("/",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl" 
+      })
+      .when("/doctors",{
+          templateUrl: "partials/doctors.html",
+          controller: "doctorsAndPatientsCtrl"
+
+      })
+      .when("/patients",{
+          templateUrl: "partials/patients.html",
+          controller:"doctorsAndPatientsCtrl"
+      })
+      .otherwise("/");
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/controllers/doctorsAndPatientsCtrl.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.controller("doctorsAndPatientsCtrl",function($scope, routeFactory){
+      routeFactory.getDoctors()
+      .then((docObjects)=>{
+          $scope.doctors = docObjects;
+          let keys = Object.keys(docObjects);
+          console.log("keys",keys);
+
+
+          console.log("doc Objects", docObjects);
+
+          docObjects.forEach( (object)=> console.log(object,"object"));
+      });
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/factories/drPatFactory.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  "use strict";
+
+  module.factory("routeFactory",function(FBUrl, $q,$http){
+      return{
+          getDoctors : () =>{
+              console.log(FBUrl, "fbURL");
+              return $q((resolve,reject)=>{
+                  $http
+                  .get(`${FBUrl}/doctors.json`)
+                  .then((data)=>{
+                      console.log("data",data.data);
+                      resolve(data.data);
+                  });
+          });
+       }
+      };
+  });
+
+}) (angular.module ('DoctorsAndPatients', ['ngRoute']));
+
+
